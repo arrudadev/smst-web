49ee6062d863f6a4d9a3c54dc346a1f0fe6730a4
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2022-09-16 12:59:27 -0300

    setup eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <h1>hello world</h1>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
